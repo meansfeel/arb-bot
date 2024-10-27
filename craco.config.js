ff53567572baa6cc90a79a2e1442d57db992b0ca
meansfeel <meansfeel@gmail.com>
2024-10-28 02:53:04 +0800

    Update message

--- craco.config.js
+++ craco.config.js
@@ -4,45 +4,31 @@ const path = require('path');
 module.exports = {
   // 这里可以添加你的 CRACO 配置
   webpack: {
-    configure: (webpackConfig) => {
-      // 添加 fallback
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        "stream": require.resolve("stream-browserify"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "crypto": require.resolve("crypto-browserify"),
-        "buffer": require.resolve("buffer"),
-        "process": require.resolve("process/browser"),
-      };
-
-      // 使用 ProvidePlugin
-      webpackConfig.plugins = [
-        ...webpackConfig.plugins,
+    configure: {
+      resolve: {
+        fallback: {
+          process: require.resolve('process/browser.js'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          stream: require.resolve('stream-browserify'),
+        },
+      },
+      plugins: [
         new webpack.ProvidePlugin({
-          process: 'process/browser',
-          Buffer: ['buffer', 'Buffer'],
-        })
-      ];
-
-      // 設置 performance hints
-      webpackConfig.performance = {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-      };
-
-      return webpackConfig;
-    }
+          process: 'process/browser.js',
+        }),
+      ],
+    },
   },
   babel: {
     presets: [
-      '@babel/preset-env',
-      '@babel/preset-react'
+      ['@babel/preset-env', { loose: true }],
+      ['@babel/preset-react', { runtime: 'automatic' }]
     ],
     plugins: [
-      '@babel/plugin-proposal-private-property-in-object'
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }]
     ]
   },
   jest: {
