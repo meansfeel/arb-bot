e7fd58043f7ad9b12311f62d15e0d656fb079428
meansfeel <72459344+meansfeel@users.noreply.github.com>
2024-10-28 23:37:11 +0800

    更新 config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,14 @@
 const webpack = require('webpack');
 
+// config-overrides.js
 module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.js$/,
-    use: ["source-map-loader"],
-    enforce: "pre"
-  });
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "url": require.resolve("url/"),
+    "stream": require.resolve("stream-browserify")
+  };
   return config;
 }
+
