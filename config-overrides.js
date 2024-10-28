7641e340100f1205b931bb7a8f3150587f59521a
meansfeel <72459344+meansfeel@users.noreply.github.com>
2024-10-28 23:52:46 +0800

    更新 config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,14 @@
 const webpack = require('webpack');
 
-// config-overrides.js
 module.exports = function override(config, env) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
+    "os": require.resolve("os-browserify/browser"),
     "http": require.resolve("stream-http"),
     "https": require.resolve("https-browserify"),
-    "url": require.resolve("url/"),
     "stream": require.resolve("stream-browserify")
   };
+  
   return config;
 }
 
